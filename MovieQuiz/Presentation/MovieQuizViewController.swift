4f0b9cd6936e5b3867015134ff70c0eead61a053
Shliakhin Sergei <sshlyahin@yahoo.com>
2022-10-14 14:18:44 +0300

    feat: Add colors and fonts

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -7,6 +7,16 @@ final class MovieQuizViewController: UIViewController {
     }
 }
 
+// MARK: - Color
+extension UIColor {
+    static var ypBlack: UIColor { UIColor(named: "black")! }
+    static var ypGray: UIColor { UIColor(named: "gray")! }
+    static var ypGreen: UIColor { UIColor(named: "green")! }
+    static var ypRed: UIColor { UIColor(named: "red")! }
+    static var ypWhite: UIColor { UIColor(named: "white")! }
+    static var ypBackground: UIColor { UIColor(named: "background")! }
+}
+
 /*
  Mock-данные
  
