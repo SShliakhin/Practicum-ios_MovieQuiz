f81ea0fa6162be8d2b37ca2406c735b7dba83474
Shliakhin Sergei <sshlyahin@yahoo.com>
2022-11-12 19:20:45 +0300

    style: Delete unused func

--- MovieQuiz/Presentation/AlertPresenter.swift
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -7,28 +7,7 @@
 
 import UIKit
 
-final class AlertPresenter {
-    static func displayResult(
-        _ model: QuizResultsViewModel,
-        over vc: UIViewController,
-        completion: @escaping () -> Void
-    ) {
-        let alert = UIAlertController(
-            title: model.title,
-            message: model.text,
-            preferredStyle: .alert
-        )
-        
-        let action = UIAlertAction(title: model.buttonText, style: .default) { _ in
-            completion()
-        }
-        alert.addAction(action)
-        
-        vc.present(alert, animated: true)
-    }
-}
-
-extension AlertPresenter: AlertPresenterProtocol {
+final class AlertPresenter: AlertPresenterProtocol {
     func displayResult(_ model: AlertModel, over vc: UIViewController) {
         let alert = UIAlertController(
             title: model.title,
