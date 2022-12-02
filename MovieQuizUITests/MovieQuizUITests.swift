15d830e2595b20c7da1d8c576b67638f72354027
Shliakhin Sergei <sshlyahin@yahoo.com>
2022-12-02 04:04:46 +0300

    feat: Add UI tests

---
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -0,0 +1,30 @@
+//
+//  MovieQuizUITests.swift
+//  MovieQuizUITests
+//
+//  Created by SERGEY SHLYAKHIN on 02.12.2022.
+//
+
+import XCTest
+
+class MovieQuizUITests: XCTestCase {
+    // swiftlint:disable:next implicitly_unwrapped_optional
+    var app: XCUIApplication!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        
+        app = XCUIApplication()
+        app.launch()
+        
+        // это специальная настройка для тестов: если один тест не прошёл,
+        // то следующие тесты запускаться не будут; и правда, зачем ждать?
+        continueAfterFailure = false
+    }
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        
+        app.terminate()
+        app = nil
+    }
+}
